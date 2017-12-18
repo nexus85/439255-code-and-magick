16a2a4f173c32f47c406bb93f3a219506fdda4b5
TemLove <temnova.hi@gmail.com>
2017-12-19 03:17:10 +0700

    Меняет реализацию модуля colorize

--- js/colorize.js
+++ js/colorize.js
@@ -1,37 +1,21 @@
 'use strict';
 (function () {
-  var setupWizard = {
-    coatColor: window.settings.Wizard.COAT_COLORS[0],
-    eyesColor: window.settings.Wizard.EYES_COLORS[0],
-    fireballColor: window.settings.Wizard.FIREBALL_COLORS[0]
+  var colorizeElement = function (element, colors, setColorMethod) {
+    var index = 1;
+    return function () {
+      setColorMethod(element, colors[index]);
+      index = index + 1 < colors.length ? index + 1 : 0;
+    };
   };
-  var changeColor = function (property, source) {
-    for (var i = 0; i < source.length; i++) {
-      if (source[i] === setupWizard[property]) {
-        break;
-      }
-    }
-    i = (i + 1 < source.length) ? i + 1 : 0;
-    setupWizard[property] = source[i];
+  var setFillColor = function (element, color) {
+    element.style.fill = color;
   };
-  var changeCoatColor = function () {
-    changeColor('coatColor', window.settings.Wizard.COAT_COLORS);
-    window.elements.wizardCoatInput.value = setupWizard.coatColor;
-    window.elements.setupWizardCoat.setAttribute('style', 'fill: ' + setupWizard.coatColor);
-  };
-  var changeEyesColor = function () {
-    changeColor('eyesColor', window.settings.Wizard.EYES_COLORS);
-    window.elements.wizardEyesInput.value = setupWizard.eyesColor;
-    window.elements.setupWizardEyes.setAttribute('style', 'fill: ' + setupWizard.eyesColor);
-  };
-  var changeFireballColor = function () {
-    changeColor('fireballColor', window.settings.Wizard.FIREBALL_COLORS);
-    window.elements.setupFireballInput.value = setupWizard.fireballColor;
-    window.elements.setupFireballWrap.setAttribute('style', 'background-color: ' + setupWizard.fireballColor);
+  var setBackgroundColor = function (element, color) {
+    element.style.backgroundColor = color;
   };
   window.colorize = {
-    changeCoatColor: changeCoatColor,
-    changeEyesColor: changeEyesColor,
-    changeFireballColor: changeFireballColor
+    colorizeElement: colorizeElement,
+    setFillColor: setFillColor,
+    setBackgroundColor: setBackgroundColor
   };
 })();
